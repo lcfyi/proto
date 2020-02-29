b1f3df4d5991c2e7580e2360bd89c4c5a87ca8c3
Leslie Cheng <leslie.cheng5@gmail.com>
2020-02-29 08:32:58 -0800

    Add all values.

--- src/proto.ino
+++ src/proto.ino
@@ -1,10 +1,14 @@
+#include <PublishQueueAsyncRK.h>
+
 SYSTEM_THREAD(ENABLED);
 
 void resetValues();
 String createPayload(String t, String data);
-String payloadTime;
+float getVal(uint8_t high, uint8_t low, float factor, float offset);
 void readSerialData();
 
+String payloadTime;
+unsigned long RATE = 3000;
 unsigned long time_ms;
 // As listed on the EcoEFI documentation
 float rpm;
@@ -21,6 +25,9 @@ float fuellvl;
 float baro;
 float fuel_consumption;
 
+uint8_t publishQueueRetainedBuffer[8192];
+PublishQueueAsync publishQueue(publishQueueRetainedBuffer, sizeof(publishQueueRetainedBuffer));
+
 void setup() {
   Serial1.begin(115200, SERIAL_8N1);
   Serial.begin(9600);
@@ -31,13 +38,15 @@ void setup() {
 void loop() {
   readSerialData();
   unsigned long curr_time_ms = millis();
-  if (curr_time_ms - time_ms >= 2000) {
+  if (curr_time_ms - time_ms >= RATE) {
     time_ms = millis();
     if (Particle.connected()) {
-      Particle.publish("PROTO-RPM", createPayload(payloadTime, String(rpm)), PUBLIC, WITH_ACK);
-      Particle.publish("PROTO-IAT", createPayload(payloadTime, String(iat)), PUBLIC, WITH_ACK);
-      Particle.publish("PROTO-ECT", createPayload(payloadTime, String(ect)), PUBLIC, WITH_ACK);
-      Particle.publish("PROTO-UBADC", createPayload(payloadTime, String(ubadc)), PUBLIC, WITH_ACK);
+      publishQueue.publish("PROTO-RPM", createPayload(payloadTime, String(rpm)), PUBLIC, WITH_ACK);
+      publishQueue.publish("PROTO-O2S", createPayload(payloadTime, String(o2s)), PUBLIC, WITH_ACK);
+      publishQueue.publish("PROTO-ECT", createPayload(payloadTime, String(ect)), PUBLIC, WITH_ACK);
+      publishQueue.publish("PROTO-IAT", createPayload(payloadTime, String(iat)), PUBLIC, WITH_ACK);
+      publishQueue.publish("PROTO-SPARK", createPayload(payloadTime, String(spark_val)), PUBLIC, WITH_ACK);
+      publishQueue.publish("PROTO-UBADC", createPayload(payloadTime, String(ubadc)), PUBLIC, WITH_ACK);
     }
   }
 }
@@ -65,11 +74,15 @@ String createPayload(String t, String data) {
   return t;
 }
 
+float getVal(uint8_t high, uint8_t low, float factor, float offset) {
+  return (float)((int)high * 256 + (int)low) * factor + offset;
+}
+
 void readSerialData() {
   // Clear any old data from the buffer
-  while (Serial1.available()) {
-    Serial1.read();
-  }
+  // while (Serial1.available()) {
+  //   Serial1.read();
+  // }
   while (Serial1.available() < 27) {
     continue;
   }
@@ -85,25 +98,14 @@ void readSerialData() {
   if (header1 == 0x80 && header2 == 0x8F && header3 == 0xEA && dataFieldLength == 0x16 && serviceId == 0x50) {
     Serial.println("Header verified");
   }
-  rpm = (float)((int)buffer[6] * 256 + (int)buffer[7]) * 0.25;
-  ect = (float)((int)buffer[12] * 256 + (int)buffer[13]) - 40;
-  iat = (float)((int)buffer[14] * 256 + (int)buffer[15]) - 40;
-  ubadc = (float)((int)buffer[24] * 256 + (int)buffer[25]) * 0.00625;
-  fuelpw1 = (float)((int)buffer[20] * 256 + (int)buffer[21]) * 0.001;
-  // Serial.println(rpm);
-  // Serial.println(ect);
-  // Serial.println(iat);
-  // Serial.println(ubadc);
-  // Serial.println(fuelpw1);
-  // // map_val = (float)(buffer[2] * 256 + buffer[3]) * 0.0039;
-  // tps = (float)(buffer[4] * 256 + buffer[5]) * 0.0015;
-  // ect = (float)(buffer[6] * 256 + buffer[7]) * 1.25 - 40;
-  // iat = (float)(buffer[8] * 256 + buffer[9]) * 1.25 - 40;
-  // o2s = (float)(buffer[10] * 256 + buffer[11]) * 0.0012;
-  // spark_val = (float)(buffer[12] * 256 + buffer[13]) * 0.75;
-  // fuelpw1 = (float)(buffer[14] * 256 + buffer[15]) * 0.01;
-  // ubadc = (float)(buffer[18] * 256 + buffer[19]) * 0.00625;
-  // Serial1.read();
-  // Serial.println(timestamp)
-
+  rpm = getVal(buffer[6], buffer[7], 0.25, 0.0);
+  map_val = getVal(buffer[8], buffer[9], 0.0039, 0.0);
+  tps = getVal(buffer[10], buffer[11], 0.0015, 0.0);
+  ect = getVal(buffer[12], buffer[13], 1, -40.0);
+  iat = getVal(buffer[14], buffer[15], 1, -40.0);
+  o2s = getVal(buffer[16], buffer[17], 0.0048, 0.0);
+  spark_val = getVal(buffer[18], buffer[19], 0.5, 0.0);
+  fuelpw1 = getVal(buffer[20], buffer[21], 0.001, 0.0);
+  fuelpw2 = getVal(buffer[22], buffer[23], 0.001, 0.0);
+  ubadc = getVal(buffer[24], buffer[25], 0.00625, 0.0);
 }
